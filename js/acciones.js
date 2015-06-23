9d0c3887e65689a13973f68a64419052505d70e4
Tecalero <altexe13@gmail.com>
2015-06-23 18:15:33 -0500

    uno
    Se subio por primera vez

---
+++ js/acciones.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function(e) {
+<!--document.addEventListener("deviceready",function(){
+	 document.addEventListener("deviceready",function(){
+    $('#btnvibrar').on('tap',function() {
+		navegator.vibrate(1000);
+	}); //tap a btnvidrar
+}); 
+<!--});
+})
